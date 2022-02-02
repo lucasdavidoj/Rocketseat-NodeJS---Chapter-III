1f051a454ce8e4fadf4aa7590689e2e547cbdbde
Lucas David <lucas.david.oj@gmail.com>
2022-02-02 10:33:17 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Rocketseat NodeJS - Chapter III
+
+Projeto desenvolvido durante os estudos no Chapter III do bootcamp de NodeJS da Rocketseat.
+
+
+
+
---
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -0,0 +1,13 @@
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+import { ImportCategoryController } from "./importCategoryController";
+import { ImportCategoryUseCase } from "./importCategoryUseCase";
+
+const importCategoryRepository = null;
+const importCategoryUseCase = new ImportCategoryUseCase(
+  importCategoryRepository
+);
+const importCategoryController = new ImportCategoryController(
+  importCategoryUseCase
+);
+
+export { importCategoryController };
