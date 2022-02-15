449e56daee8c7cc16a30a5f569c6148e079651d0
Lucas David <lucas.david.oj@gmail.com>
2022-02-15 16:17:56 -0300

    Criptografrar senha

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,7 +1,9 @@
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+import { User } from "../entities/Users";
 
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
+  findByEmail(email: string): Promise<User>;
 }
 
 export { IUsersRepository };
